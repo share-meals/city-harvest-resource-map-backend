126788f22ec3f5a26949c63c094ba6cadb333bf5
Jon Chin <jon@sharebyteaching.org>
2024-05-15 16:39:34 -0400

    updated

--- src/log-feature-click.ts
+++ src/log-feature-click.ts
@@ -2,25 +2,29 @@ import {HttpFunction} from '@google-cloud/functions-framework';
 import {BigQuery} from '@google-cloud/bigquery';
 
 export const logFeatureClick: HttpFunction = async (req, res) => {
-  res.set('Access-Control-Allow-Origin', "*")
-  res.set('Access-Control-Allow-Methods', 'GET, POST');
-  res.set('Access-Control-Allow-Headers', 'Content-Type');
-  const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  const bigquery = new BigQuery();
-  try{
-    await bigquery
-    .dataset('resourceMap')
-    .table('featureClicks')
-    .insert({
-      featureId: req.body.id,
-      ipAddress,
-      lat: req.body.lat,
-      lng: req.body.lng,
-      timestamp: new Date(),
-    });
-  }catch(error){
-    // @ts-ignore
-    console.log(error.errors[0]);
+  if(req.method === 'OPTIONS'){
+    res.set('Access-Control-Allow-Origin', "*")
+    res.set('Access-Control-Allow-Methods', 'GET, POST');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.status(204).send('');
+  }else{
+    const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const bigquery = new BigQuery();
+    try{
+      await bigquery
+      .dataset('resourceMap')
+      .table('featureClicks')
+      .insert({
+	featureId: req.body.id,
+	ipAddress,
+	lat: parseFloat(req.body.lat.toFixed(8)),
+	lng: parseFloat(req.body.lng.toFixed(8)),
+	timestamp: new Date(),
+      });
+    }catch(error){
+      // @ts-ignore
+      console.log(error);
+    }
+    res.status(200).send('OK');
   }
-  res.send('OK');
 };
--- src/log-geocode.ts
+++ src/log-geocode.ts
@@ -2,25 +2,30 @@ import {HttpFunction} from '@google-cloud/functions-framework';
 import {BigQuery} from '@google-cloud/bigquery';
 
 export const logGeocode: HttpFunction = async (req, res) => {
-  res.set('Access-Control-Allow-Origin', "*")
-  res.set('Access-Control-Allow-Methods', 'GET, POST');
-  res.set('Access-Control-Allow-Headers', 'Content-Type');
-  const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  const bigquery = new BigQuery();
-  try{
-    await bigquery
-    .dataset('resourceMap')
-    .table('geocodes')
-    .insert({
-      address: req.body.address,
-      ipAddress,
-      lat: req.body.lat,
-      lng: req.body.lng,
-      timestamp: new Date(),
-    });
-  }catch(error){
-    // @ts-ignore
-    console.log(error.errors[0]);
+  if(req.method === 'OPTIONS'){
+    res.set('Access-Control-Allow-Origin', "*")
+    res.set('Access-Control-Allow-Methods', 'GET, POST');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.status(204).send('');
+  }else{
+
+    const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const bigquery = new BigQuery();
+    try{
+      await bigquery
+      .dataset('resourceMap')
+      .table('geocodes')
+      .insert({
+	address: req.body.address,
+	ipAddress,
+	lat: parseFloat(req.body.lat.toFixed(8)),
+	lng: parseFloat(req.body.lng.toFixed(8)),
+	timestamp: new Date(),
+      });
+    }catch(error){
+      // @ts-ignore
+      console.log(error.errors[0]);
+    }
+    res.status(200).send('OK');
   }
-  res.send('OK');
 };
